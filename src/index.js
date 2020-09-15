2563cf9e6925c5ce9e1975ccc51d4a9f1dbc9df8
OlgaR-k <68254567+OlgaR-k@users.noreply.github.com>
2020-09-16 01:52:38 +0300

    init: start task morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,41 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    return getLetters(expr);
+}
+
+function getArray(expr) {
+    let letters = [];
+    for ( let i = 0; i < expr.length; i +=10 ){
+        letters.push( expr.slice(i, i +10) ); 
+    }
+    return letters;
+}
+
+function getLetters(expr) {
+    let arrNum = getArray(expr);
+    let letters = [];
+    let dota = '10';
+    let dash = '11';
+    let space = '**********';
+    let key;
+    for ( let i = 0; i < arrNum.length; i++ ) {
+        if ( arrNum[i] == space ) {
+            letters.push(' ');
+            continue;
+        }
+        key = '';
+        for ( let j = 0; j < 10; j+=2 ) {
+            if ( arrNum[i][j] + arrNum[i][j+1] == dota ) {
+                key += '.';
+            } else if ( arrNum[i][j] + arrNum[i][j+1] == dash ) {
+                key += '-';
+            } 
+        }
+
+        letters.push( MORSE_TABLE[key] );
+    }
+    return letters.join('');
 }
 
 module.exports = {
